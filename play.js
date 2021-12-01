5d05e6caae97bc65fc14833444919d8559d55387
Ren <cassandra.chen@uoit.net>
2021-12-01 22:56:02 +0000

    key added to exit and input for movement

--- play.js
+++ play.js
@@ -1,4 +1,46 @@
 const connect = require('./client')
 
 console.log("Connecting ...");
-connect;
+connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  // process.stdin.on('data', () => {
+  //   if(key === 'w'){
+  //     setTimeout(() => {
+  //       process.stdin.write('Move: up')
+  //     }, 50)
+  //   }
+  //   else if(key === 's'){
+  //     setTimeout(() => {
+  //       process.stdin.write('Move: down')
+  //     }, 50)
+  //   }
+  //   else if(key === 'a'){
+  //     setTimeout(() => {
+  //       process.stdin.write('Move: left')
+  //     }, 50)
+  //   }
+  //   else if(key === 'd'){
+  //     setTimeout(() => {
+  //       process.stdin.write('Move: right')
+  //     })
+  //   }
+  //   else if (key === '\u0003') {
+  //     process.exit();
+  //   }
+  // })
+};
+
+setupInput()
